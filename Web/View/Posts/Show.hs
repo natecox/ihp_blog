e7f957d82146c79a20d73d5be53abcce43725ded
Nathan Cox <ncox@covermymeds.com>
2022-10-27 14:32:53 -0700

    Render posts with markdown

---
+++ Web/View/Posts/Show.hs
@@ -0,0 +1,23 @@
+module Web.View.Posts.Show where
+import Web.View.Prelude
+import qualified Text.MMark as MMark
+
+data ShowView = ShowView { post :: Post }
+
+instance View ShowView where
+    html ShowView { .. } = [hsx|
+        {breadcrumb}
+        <h1>{post.title}</h1>
+        <p>{post.createdAt |> timeAgo}</p>
+        <div>{post.body |> renderMarkdown}</div>
+
+    |]
+        where
+            breadcrumb = renderBreadcrumb
+                            [ breadcrumbLink "Posts" PostsAction
+                            , breadcrumbText "Show Post"
+                            ]
+            renderMarkdown text =
+              case text |> MMark.parse "" of
+                Left error -> "Something went wrong"
+                Right markdown -> MMark.render markdown |> tshow |> preEscapedToHtml
