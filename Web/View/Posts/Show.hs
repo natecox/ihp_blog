408da019baba36edd619c0183cf2a1424135ef15
Nathan Cox <natecox@Nathans-MacBook-Pro.local>
2022-10-27 19:45:45 -0700

    Add post comments

--- Web/View/Posts/Show.hs
+++ Web/View/Posts/Show.hs
@@ -2,7 +2,7 @@ module Web.View.Posts.Show where
 import Web.View.Prelude
 import qualified Text.MMark as MMark
 
-data ShowView = ShowView { post :: Post }
+data ShowView = ShowView { post :: Include "comments" Post }
 
 instance View ShowView where
     html ShowView { .. } = [hsx|
@@ -11,13 +11,24 @@ instance View ShowView where
         <p>{post.createdAt |> timeAgo}</p>
         <div>{post.body |> renderMarkdown}</div>
 
+        <a href={NewCommentAction post.id}>Add Comment</a>
+        <div>{forEach post.comments renderComment}</div>
     |]
         where
             breadcrumb = renderBreadcrumb
-                            [ breadcrumbLink "Posts" PostsAction
-                            , breadcrumbText "Show Post"
-                            ]
+                         [ breadcrumbLink "Posts" PostsAction
+                         , breadcrumbText "Show Post"
+                         ]
+
             renderMarkdown text =
               case text |> MMark.parse "" of
                 Left error -> "Something went wrong"
                 Right markdown -> MMark.render markdown |> tshow |> preEscapedToHtml
+
+            renderComment comment =
+              [hsx|
+                  <div class="mt-4">
+                  <h5>{comment.author}</h5>
+                  <p>{comment.body}</p>
+                  </div>
+                  |]
